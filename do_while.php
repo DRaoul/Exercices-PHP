431228bbe85ee6d4d66b5bf94b8d393a62a35dc2
Raoul Dohou <raouldohou@gmail.com>
2025-08-22 10:27:44 +0100

    exercice PHP

---
+++ do_while.php
@@ -0,0 +1,18 @@
+<?php
+
+$t = 0 ;
+
+$tableau = array('Marc', 'Yves', 'Gisel');
+
+foreach($tableau as $name){
+    echo $name . ' votre numero de table est '. ($t+1)."<br/>" ;
+    $t++;
+
+}
+
+// do {
+//     echo $name' Votre note est ' .($t+1). "<br/>";
+//     $t++;
+
+
+// } while ($t <9);
--- index.php
+++ index.php
@@ -4,8 +4,9 @@
 
 $user = array('Nicols', 'Geerge', 'Emmanuel', 'Marc');
 
-for ($i = 0; $i <5; $i++) {
-    echo 'Votre nom est : '. $user[$i]. "<br/>";
 
+foreach( $user as $note){
+
+    echo $note . "\n". "<br/>";
 
 }
